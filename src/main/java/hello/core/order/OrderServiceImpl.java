7fde0c2f17e020eabd0010ef51b49372bc5e7d94
EastHShin <xldk78@gmail.com>
2021-09-20 15:12:59 +0900

    bean scope

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,30 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor    //final이 붙으면 필수값, final 붙은거를 가지고 생성자 만들어줌
 public class OrderServiceImpl implements OrderService {
 
 
     /*@Autowired -> 필드 주입은 외부에서 변경이 불가능해 테스트가 힘들다,
     그러니 사용하지 말자!
      */
+    // 생성자 주입에서만 final 사용 가능
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     @Autowired  //생성자가 1개일때는 @Autowired 안붙여도 의존관계 주입됨
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
